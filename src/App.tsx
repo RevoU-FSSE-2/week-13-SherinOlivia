db0a7c1576490d28716f7191d3bd8fba410b6cb3
Sherin Olivia <SOChronicle@gmail.com>
2023-09-19 07:32:28 +0700

    chore: test register and login form

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState } from 'react';
 import './App.css'
+import { Login, Register } from './containers'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [step, setStep] = useState<number>(1);
+
+  const handleNext = () => {
+    if(step === 1 || step === 2) {
+        setStep((prevStep) => prevStep+1);
+    }
+
+    return
+  }
+
+  const handlePrev = () => {
+    if(step === 2 || step === 3) {
+        setStep((prevStep) => prevStep - 1);
+    }
+
+    return
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {step === 1 && (
+        <Login />    
+      )}
+      {step === 2 && (
+        <Register />  
+      )}
     </>
   )
 }
 
 export default App
+
