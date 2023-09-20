9b6919b3fabc3204d1b4a943e52882fab99aa6d3
Sherin Olivia <SOChronicle@gmail.com>
2023-09-21 06:32:16 +0700

    feat: created new page/path: CategoryList

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import './App.css'
 import  {BrowserRouter, Route, Routes } from 'react-router-dom'
-import { Login, Register } from './containers'
+import { CategoryList, Login, Register } from './containers'
 
 function App() {
   
   return (
     <BrowserRouter>
       <Routes>
+        <Route path='/' element={<CategoryList />} />
         <Route path='/login' element={<Login />} /> 
         <Route path='/register' element={<Register />} />
       </Routes>
