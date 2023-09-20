a8e29c583fa781d2b3f8771ea4d53c447e1e2d72
Sherin Olivia <SOChronicle@gmail.com>
2023-09-20 07:30:03 +0700

    feat: component/page Login

--- src/containers/Login/index.tsx
+++ src/containers/Login/index.tsx
@@ -1,55 +1,76 @@
 import React from 'react';
 import { LockOutlined, UserOutlined } from '@ant-design/icons';
-import { Button, Checkbox, Form, Input } from 'antd';
+import { Button, Card, Col, Row, Input, Form as AntForm } from 'antd';
+import { Formik, Form, Field, ErrorMessage  } from 'formik';
+import * as Yup from 'yup'
 import { TextLevel } from '../../components';
+import styles from './Login.module.css'
+import { Link, useNavigate } from 'react-router-dom';
+
+interface LoginInfo {
+    email: string;
+    password: string;
+}
+
+
+const validationSchema = Yup.object().shape({
+    email: Yup.string().email("Invalid Email!").required('Please Enter Your Email'),
+    password: Yup.string().min(8, "Password must have at least 8 characters")
+    .required('Please Enter Your Password')
+})
 
 const Login: React.FC = () => {
-  const onFinish = (values: object) => {
-    console.log('Received values of Login form: ', values);
-  };
+  const navigate = useNavigate()
+
+  const handleSubmit = (values: LoginInfo) => {
+    console.log(`Successfully logged in`, values)
+    navigate('/');
+  }
 
   return (
-    <Form
-      name="normal_login"
-      className="login-form"
-      initialValues={{ remember: true }}
-      onFinish={onFinish}
-    >
-      <TextLevel level={3} content={"Login"}/>
+    <Row className={styles.wrapper}>
+    <Col span={8}></Col>
+    <Col span={8} className={styles.body}>
+        <Card className={styles.card}>
+            <Formik 
+            initialValues = {{ email: "", password: ""}}
+            validationSchema={validationSchema}
+            onSubmit={handleSubmit}>
+                <Form name="basic" autoComplete="off">
+                    <TextLevel level={3} content={"Login"}/>
+                     
+                    <AntForm.Item label="Email" name="email">
+                    <Field prefix={<UserOutlined className="site-form-item-icon" />} 
+                    name="email" as={Input} placeholder="Enter Your Email" />
+                    
+                    <div className={styles.error}>
+                        <ErrorMessage name="email" />
+                    </div>
+                    </AntForm.Item>
+                
+                    <AntForm.Item label="Password" name="password">
+                    <Field prefix={<LockOutlined className="site-form-item-icon" />} 
+                    name="password" as={Input} placeholder="Enter Your Password" />
 
-      <Form.Item
-        name="username"
-        rules={[{ required: true, message: 'Please input your Username!' }]}
-      >
-        <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="Username" />
-      </Form.Item>
-      <Form.Item
-        name="password"
-        rules={[{ required: true, message: 'Please input your Password!' }]}
-      >
-        <Input
-          prefix={<LockOutlined className="site-form-item-icon" />}
-          type="password"
-          placeholder="Password"
-        />
-      </Form.Item>
-      <Form.Item>
-        <Form.Item name="remember" valuePropName="checked" noStyle>
-          <Checkbox>Remember me</Checkbox>
-        </Form.Item>
+                    <div className={styles.error}>
+                        <ErrorMessage name="password" />
+                    </div>
+                    </AntForm.Item>
+                
+                    <AntForm.Item wrapperCol={{ offset: 8, span: 16 }}>
+                        <div>
+                            <Button type="primary" htmlType="submit" className="login-form-button">
+                            Log in
+                            </Button>
+                            Or <Link to={'/register'}>register now!</Link>
+                        </div>
+                    </AntForm.Item>
+                </Form>
+            </Formik>
+        </Card>
+    </Col>
+    </Row>
 
-        <a className="login-form-forgot" href="">
-          Forgot password
-        </a>
-      </Form.Item>
-
-      <Form.Item>
-        <Button type="primary" htmlType="submit" className="login-form-button">
-          Log in
-        </Button>
-        Or <a href="">register now!</a>
-      </Form.Item>
-    </Form>
   );
 };
 
