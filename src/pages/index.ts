eb5975cd6ad99600444ec12ff951ffad5f340bd5
Sherin Olivia <SOChronicle@gmail.com>
2023-09-22 06:47:16 +0700

    feat: added new export CreateNew

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,3 +1,4 @@
-export { default as Login} from './Login'
-export { default as Register} from './Register'
-export { default as CategoryList} from './CategoryList'
+export { default as Login } from './Login'
+export { default as Register } from './Register'
+export { default as CategoryList } from './CategoryList'
+export { default as CreateNew } from './CreateNew'
