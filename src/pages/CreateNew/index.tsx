6fabe6df9401e2e5acf4bd3efd8a03517fc79ece
Sherin Olivia <SOChronicle@gmail.com>
2023-09-22 06:46:57 +0700

    feat: Create New

--- src/pages/CreateNew/index.tsx
+++ src/pages/CreateNew/index.tsx
@@ -1,37 +1,42 @@
-// import { useNavigate } from "react-router-dom"
+import { useNavigate } from "react-router-dom"
+import {  CreateInfo } from "../../types";
+import { CreateNew as CreateNewComponent } from "../../components";
 
-// interface CreateInfo {
-//     name: string;
-// }
 
-// const CreateNew = () => {
-//     const navigate = useNavigate()
-//     const handleCreate = async (values: CreateInfo) => {
-//         console.log(`Successfully Created New Category:`, values)
-//         const apiUrl = "https://mock-api.arikmpt.com/api/category/create"
-      
-//         try {
-//             const response = await fetch (apiUrl, {
-//                 method: 'POST',
-//                 headers: {
-//                     'Content-Type': 'application/json'
-//                 },
-//                 body: JSON.stringify(values)
-//             })
-//             console.log(response)
-//             const data = await response.json()
-      
-//             if (response.ok){
-//                 navigate('/');
-//             } else {
-//                 alert(data.errors)
-//             }
-//         } catch (error) {
-//             alert("Failed to Create New Category...!")
-//         }
-      
-//       }
-      
-// }
+const CreateNew = () => {
+    const navigate = useNavigate()
 
-// export default CreateNew
+    const handleCreate = async (values: CreateInfo) => {
+        console.log(`Successfully Created New Category:`, values)
+        const apiUrl = import.meta.env.VITE_REACT_APP_CREATE_URL;
+      
+        try {
+            const response = await fetch (apiUrl, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    Authorization: `Bearer ${localStorage.getItem('authToken')}`
+                },
+                body: JSON.stringify(values)
+            })
+            if (response.ok) {
+                console.log(response)
+                await response.json()
+                navigate('/');  
+            } else {
+                console.log("Failed to create new category")
+                return
+            }
+   
+        } catch (error) {
+            console.error(error)
+            alert("Failed to Create New Category...!")
+        }  
+      }
+
+    return (
+        <CreateNewComponent onSubmit={handleCreate} />
+    )    
+}
+
+export default CreateNew
