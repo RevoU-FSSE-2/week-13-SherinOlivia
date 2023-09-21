1bb5cfdde3fe638b07d522a23ca7d8f9c6124a9f
Sherin Olivia <SOChronicle@gmail.com>
2023-09-22 06:46:36 +0700

    feat: updated function for delete and create new

--- src/pages/CategoryList/index.tsx
+++ src/pages/CategoryList/index.tsx
@@ -4,26 +4,14 @@ import type { ColumnsType } from 'antd/es/table';
 import { useNavigate } from 'react-router-dom';
 import { CategoryList as CategoryListComponent, TextLevel } from '../../components'
 import styles from './CategoryList.module.css'
-import { getStatus} from '../../hooks';
-
-// interface GetCategoryResponse {
-//   categories: CategoryInfo[];
-//   total: number;
-//   skip: number;
-//   limit: number;
-// }
-interface CategoryInfo {
-  id: string;
-  name: string;
-  status: boolean;
-}
-
+import { useStatus } from '../../hooks';
+import { CategoryInfo } from '../../types';
 
 const CategoryList: React.FC = () => {
   const [categories, setCategories] = useState<CategoryInfo[]>([]);
   const navigate = useNavigate();
   const apiUrl = import.meta.env.VITE_REACT_APP_BASE_URL;
-  console.log(apiUrl)
+  const getStatusText = useStatus({ status: true });
 
 const handleLogOut = () => {
   localStorage.removeItem('authToken')
@@ -50,16 +38,36 @@ const getCategory = async () => {
 }
 
 useEffect(() => {
-      const token = localStorage.getItem('authToken')
-      if(!token) {
-        navigate('/login') 
-        return
-      }
-      getCategory()
+  const token = localStorage.getItem('authToken')
+  if(!token) {
+    navigate('/login') 
+    return
+  }
+  getCategory()
 
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [])
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
 
+  // remove/delete item
+  const removeCategory = async (id: string) => {
+    const apiUrl = import.meta.env.VITE_REACT_APP_BASE_URL;
+    try {
+        const response = await fetch(`${apiUrl}${id}`, {
+          method: 'DELETE',
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem('authToken')}`
+          }
+        })
+  
+        if(response.ok) {
+            setCategories((categories) => categories.filter((category) => category.id !== id))
+            console.log("`Successfully Removed category")
+        }
+    } catch (error) {
+        console.error(error)
+    }
+  }
+  
   const columns: ColumnsType<CategoryInfo> = [
     {
       title: 'ID',
@@ -75,7 +83,7 @@ useEffect(() => {
       title: 'Status',
       dataIndex: 'is_active',
       key: 'is_active',
-      render: (_, record) => getStatus({ status: record.status })
+      render: () => getStatusText
     },
     {
       title: 'Action',
@@ -83,7 +91,7 @@ useEffect(() => {
       render: (_, record) => (
         <>
           <Button type={'primary'} onClick={() => navigate(`${apiUrl}update`)}>Edit</Button>
-          <Button type={'primary'} onClick={() => navigate(`${apiUrl}delete/${record.id}`)} danger>Delete</Button>
+          <Button type={'primary'} onClick={() => removeCategory(record.id) } danger>Delete</Button>
         </>
 
       ),
@@ -95,11 +103,11 @@ useEffect(() => {
     <>
     <div className={styles.categoryList}>
       <div className={styles.categoryTitle}>
-      <span><Button type={'primary'} >Create New</Button></span>
+      <span><Button type={'primary'} onClick={() => navigate('/add')}>Create New</Button></span>
         <TextLevel level={1} content={"List of Category"} />
         <span className={styles.logout}><Button type={'primary'} onClick={handleLogOut} danger>Log Out</Button></span>
       </div>
-      <CategoryListComponent columns={columns} data={categories}/>
+      <CategoryListComponent columns={columns} data={categories} />
     </div>
     </>
   )
