b2bc087fbb46273f35fb43ffa2c86db0fb1b64c5
Sherin Olivia <SOChronicle@gmail.com>
2023-09-23 01:30:01 +0700

    feat: updated new exports (Login, Register, CategoryForm)

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,5 @@
-export {default as TextLevel} from './TextLevel'
-export {default as CategoryList} from './CategoryList'
-export { default as CreateNew } from './CreateNew'
+export { default as TextLevel } from './TextLevel'
+export { default as CategoryList } from './CategoryList'
+export { default as CategoryForm } from './CategoryForm'
+export { default as Login } from './Login'
+export { default as Register } from './Register'
