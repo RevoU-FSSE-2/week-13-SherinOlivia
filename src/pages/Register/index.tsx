019bafd8db9251fea9dafee5ef6b094584fe93ba
Sherin Olivia <SOChronicle@gmail.com>
2023-09-23 01:34:21 +0700

    fix: separated Register Form from Register page

--- src/pages/Register/index.tsx
+++ src/pages/Register/index.tsx
@@ -1,37 +1,13 @@
-import React from 'react';
-import { SmileOutlined, LockOutlined, UserOutlined } from '@ant-design/icons';
-import { Button, Card, Col, Row, Input, Form as AntForm } from 'antd';
-import { Formik, Form, Field, ErrorMessage,  } from 'formik';
-import * as Yup from 'yup'
-import { TextLevel } from '../../components';
-import styles from './Register.module.css'
 import { useNavigate } from 'react-router-dom';
 import { RegisterInfo } from '../../types';
+import { Register as RegisterForm } from '../../components';
 
-const passwordValidationError = (str: string) => {
-    return `Your Password must have at least 1 ${str} character`;
-}
-
-const validationSchema = Yup.object().shape({
-    name: Yup.string().required('Please Enter Your Name'),
-    email: Yup.string().email("Invalid Email!").required('Please Enter Your Email'),
-    password: Yup.string().min(8, "Password must have at least 8 characters")
-    .matches(/[0-9]/, passwordValidationError("digit"))
-    .matches(/[a-z]/, passwordValidationError("lowercase"))
-    .matches(/[A-Z]/, passwordValidationError("uppercase"))
-    .required('Please Enter Your Password')
-})
-
-  const Register: React.FC = () => {
+  const Register = () => {
     const navigate = useNavigate()
 
-    // const handleSubmit = (values: RegisterInfo) => {
-    //     console.log(`Successfully registered!`,values)
-    //     navigate('/login')
-    //   }
       const handleRegister = async (values: RegisterInfo) => {
         console.log(`Successfully Registered..!`, values)
-        const apiUrl = "https://mock-api.arikmpt.com/api/user/register"
+        const apiUrl = import.meta.env.VITE_REACT_APP_REGISTER_URL
     
         try {
             const response = await fetch (apiUrl, {
@@ -56,54 +32,7 @@ const validationSchema = Yup.object().shape({
       }
 
       return (
-        <Row className={styles.wrapper}>
-            <Col span={8}></Col>
-            <Col span={8} className={styles.body}>
-                <Card className={styles.card}>
-                    <Formik 
-                    initialValues = {{name: "", email: "", password: ""}}
-                    validationSchema={validationSchema}
-                    onSubmit={handleRegister}>
-                        <Form name="basic" autoComplete="off">
-                            <TextLevel level={3} content={"Register"}/>
-                    
-                            <AntForm.Item label="Name">
-                            <Field prefix={<SmileOutlined className="site-form-item-icon" />} 
-                            name="name" as={Input} placeholder="Enter Your Name" />
-                            
-                            <div className={styles.error}>
-                                <ErrorMessage name="name" />
-                            </div>
-                            </AntForm.Item>
-                    
-                            <AntForm.Item label="Email" name="email">
-                            <Field prefix={<UserOutlined className="site-form-item-icon" />} 
-                            name="email" as={Input} placeholder="Enter Your Email" />
-                            
-                            <div className={styles.error}>
-                                <ErrorMessage name="email" />
-                            </div>
-                            </AntForm.Item>
-                        
-                            <AntForm.Item label="Password" name="password">
-                            <Field prefix={<LockOutlined className="site-form-item-icon" />} 
-                            name="password" as={Input} placeholder="Enter Your Password" />
-
-                            <div className={styles.error}>
-                                <ErrorMessage name="password" />
-                            </div>
-                            </AntForm.Item>
-                        
-                            <AntForm.Item wrapperCol={{ offset: 8, span: 16 }}>
-                            <Button type="primary" htmlType="submit">
-                                Register
-                            </Button>
-                            </AntForm.Item>
-                        </Form>
-                    </Formik>
-                </Card>
-            </Col>
-        </Row>
+        <RegisterForm onSubmit={handleRegister} />
       )
  
     };
